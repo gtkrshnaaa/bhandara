0126ec15b71003662d2088b20b584fa19213a99a
gtkrshnaaa <gtkrshnaaa@gmail.com>
2025-12-29 15:51:50 +0700

    Register public storefront routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// GROUP: PUBLIC STOREFRONT (Tenant Aware)
+Route::prefix('u/{shop_slug}')->name('storefront.')->group(function () {
+    Route::get('/', [\App\Http\Controllers\Storefront\CatalogController::class, 'index'])->name('home');
+});
