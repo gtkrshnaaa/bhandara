5d4149d821b9b37f997422545075981fb5aa9550
gtkrshnaaa <gtkrshnaaa@gmail.com>
2025-12-29 15:44:05 +0700

    Create Catalog.php

---
+++ app/Filament/App/Clusters/Catalog.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\App\Clusters;
+
+use Filament\Clusters\Cluster;
+
+class Catalog extends Cluster
+{
+    protected static ?string $navigationIcon = 'heroicon-o-squares-2x2';
+}
