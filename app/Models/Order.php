8321b1a2983e8078841d17065990b4740144a0ef
gtkrshnaaa <gtkrshnaaa@gmail.com>
2025-12-29 15:43:52 +0700

    Create Order model

---
+++ app/Models/Order.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'tenant_id',
+        'customer_id',
+        'number',
+        'total_price',
+        'status',
+        'payment_status',
+        'shipping_address',
+        'shipping_tracking_number',
+        'notes',
+    ];
+
+    protected $casts = [
+        'total_price' => 'decimal:2',
+    ];
+
+    public function tenant(): BelongsTo
+    {
+        return $this->belongsTo(Tenant::class);
+    }
+
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function items(): HasMany
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
+    }
+}
