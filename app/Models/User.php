dc90d3c0809d6d30cc40ea67cf1b8277a753672f
gtkrshnaaa <gtkrshnaaa@gmail.com>
2025-12-29 15:59:43 +0700

    Implement HasTenants interface on User model to fix login error

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+use Filament\Models\Contracts\HasTenants;
+use Filament\Panel;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+
+class User extends Authenticatable implements HasTenants
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -53,5 +58,15 @@ class User extends Authenticatable
     public function tenants(): \Illuminate\Database\Eloquent\Relations\HasMany
     {
         return $this->hasMany(Tenant::class, 'owner_id');
+    }
+
+    public function getTenants(Panel $panel): Collection
+    {
+        return $this->tenants;
+    }
+
+    public function canAccessTenant(Model $tenant): bool
+    {
+        return $this->tenants->contains($tenant);
     }
 }
